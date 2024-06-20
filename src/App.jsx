4a4424f16e0ca390fa1c1c9bab9deb15a24b1a27
Ankit Kumar <ankit.2101243ec@iiitbh.ac.in>
2024-06-20 11:52:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ImageUpload from './components/imageUpload';
+
+function App(){
+  return (
+  <div className='image-field'>
+    <ImageUpload/>
+    <ImageUpload/>
+  </div>
+  );
+
+}
+export default App;
+
